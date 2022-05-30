a242e47fe2ec19f9e89e7ebe14b47499d80e3d8e
Roxy <hahadaxigua@gmail.com>
2022-05-30 23:38:50 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-//! Hi, my name is Aden, a backend programmer in Shanghai/Chain, who had a big
+//! Hi, my name is Aden, a backend developer in Shanghai/Chain, who had a big
 //! interest in technology, digital products.
 //!
 //! I have been using Golang as my main programming-language for two years, and
 //! Rust is my second favorite language. I'm looking for a backend-development
-//! job which using golang and rust.
+//! job which using Golang and Rust.
 //!
 //! You can find me on [GitHub](https://github.com/HaHadaxigua)
 //!
@@ -12,49 +12,39 @@
 
 // Jobs
 
-/// Bachelor of BNetwork Engineering from 2017 to 2021, in Nantong University
+/// Bachelor of BNetwork Engineering from 2017 to 2021, in Nantong University, China
 pub mod education {}
 
-/// Junior Golang backend developer.
+/// Junior Golang backend developer in Shanghai/China
 pub mod current {}
 
-// Personal Projects
+// Experience of Project
 
-/// A CLI tool that allows you to create a temporary new Rust project using
-/// cargo with already installed dependencies.
-/// [\[Repository\]](<https://github.com/rustminded/cargo-temp>)
+/// A port forward tool which writen in Golang.
+/// Support copy file, forward traffic and a easy use of private environment jumping.
 #[macro_export]
-macro_rules! cargo_temp {
+macro_rules! port_forward_tool {
     () => {};
 }
 
-/// A crate to create and handle processes on Windows (similar too `std::process`).
-/// [\[Repository\]](<https://github.com/yozhgoor/CreateProcessW>)
+/// A configuration generator which includes a type-system.
+/// xxx
+/// xxx
+/// xxx
 #[macro_export]
-macro_rules! create_process_w {
+macro_rules! configuration_generator {
     () => {};
 }
 
-/// My vision of the Rust Programming Language as an introduction to Rust.
-/// [\[Repository\]](<https://github.com/yozhgoor/intro-to-rust>)
+/// A background service for cluster setup.
+/// xxx
+/// xxx
+/// xxx
 #[macro_export]
-macro_rules! intro_to_rust {
+macro_rules! cluster_configure_tool {
     () => {};
 }
 
-/// Yet Another To-Do App in your Terminal. (Under development)
-/// [\[Repository\]](<https://github.com/yozhgoor/tui-do>)
-#[macro_export]
-macro_rules! tui_do {
-    () => {};
-}
-
-/// A Yew and Yewprint playground using wasm-run.
-/// [\[Repository\]](<https://github.com/yozhgoor/yewprint-playground>)
-#[macro_export]
-macro_rules! yewprint_playground {
-    () => {};
-}
 
 // Technical skills
 
@@ -67,7 +57,7 @@ pub const PROGRAMMING_LANGUAGES: () = ();
 /// Chinese(5.Native) English (3. Minimum Professional Proficiency).
 pub const SPOKEN_LANGUAGES: () = ();
 
-/// Git, Docker, .
+/// Git, Docker
 pub const TECHNOLOGIES: () = ();
 
 // Personal traits
