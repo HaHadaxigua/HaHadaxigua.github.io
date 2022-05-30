7bf0c55ff31769abaaafc6de8b0c7a12ee637542
Roxy <hahadaxigua@gmail.com>
2022-05-30 23:26:59 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,21 @@
-//! I'm a Former emergency response driver, who always had a big interest in
-//! programming and technology. I've been running Linux for 6 years, I tried
-//! many distributions (Ubuntu, Tails, Manjaro,...). I use Arch Linux as my
-//! daily driver because Arch gives me the opportunity to really discover what
-//! it's going on under the hood.
+//! Hi, my name is Aden, a backend programmer in Shanghai/Chain, who had a big
+//! interest in technology, digital products.
 //!
-//! I had the chance to get insight knowledge on Rust in 2020 from a friend that
-//! became my mentor. I've got the opportunity to learn Rust at RustMinded
-//! including all the technical and non-technical skills to become a developer.
+//! I have been using Golang as my main programming-language for two years, and
+//! Rust is my second favorite language. I'm looking for a backend-development
+//! job which using golang and rust.
 //!
-//! You can find me on [GitHub](https://github.com/yozhgoor) and
-//! [Twitter](https://twitter.com/yozhgoor).
+//! You can find me on [GitHub](https://github.com/HaHadaxigua)
+//!
 //! You can also send me an
-//! [email](mailto:yohan.boogaert+recruit@protonmail.com).
+//! [email](hahadaxigua@gmail.com).
 
 // Jobs
 
-/// Multiple jobs with hands on the wheel (emergency response driver, airport
-/// shuttle, taxi,...).
-pub mod between_2014_and_2020 {}
+/// Bachelor of BNetwork Engineering from 2017 to 2021, in Nantong University
+pub mod education {}
 
-/// First job with hands on the keyboard at RustMinded. Discovering the OSS
-/// world when learning how to become an Rustacean and a developer.
+/// Junior Golang backend developer.
 pub mod current {}
 
 // Personal Projects
@@ -61,76 +56,18 @@ macro_rules! yewprint_playground {
     () => {};
 }
 
-// OSS Contributions
-
-/// A CLI tool that compares a crate's public API between two different
-/// branches, shows what changed, and suggests the next version according to
-/// semver.
-/// [\[Repository\]](<https://github.com/iomentum/cargo-breaking>)
-pub struct CargoBreaking {}
-
-/// The place to be hired as an awesome Rustacean. (Under development)
-/// [\[Repository\]](<https://github.com/rustminded/recruit-rust>)
-pub struct RecruitRust {}
-
-/// French translation of the book "The Rust Programming Language".
-/// [\[Repository\]](<https://github.com/Jimskapt/rust-book-fr>)
-pub struct RustBookFr {}
-
-/// Third-I, the only camera that replicates your hearing and your point of
-/// view.
-/// [\[Repository\]](<https://github.com/BigBoySystems/third-i-frontend>) |
-/// [\[Site\]](<https://bigboysystems.com/>)
-pub struct ThirdIFrontend {}
-
-/// Bundles and tooling for Rust WASM frontend application.
-/// [\[Repository\]](<https://github.com/wasmbl/wasmbl>)
-pub struct WasmRun {}
-
-/// Customizable commands based on [xtask](https://github.com/matklad/cargo-xtask)
-/// for your WASM project.
-/// [\[Repository\]](<https://github.com/rustminded/xtask-wasm>)
-pub struct XtaskWasm {}
-
-/// A customizable helper to watch for changes in your xtask projects.
-/// [\Repository\]](<https://github.com/rustminded/xtask-watch>)
-pub struct XtaskWatch {}
-
-/// Port of [blueprintjs](<https://blueprintjs.com>) to Yew.
-/// [\[Repository\]](<https://github.com/yewprint/yewprint>)
-pub struct Yewprint {}
-
-// Non-technical skills
-
-/// Release, update and maintenance of an OSS project.
-pub enum OssProjectMaintenance {}
-
-/// Project organisation using
-/// [GitHub Project](<https://docs.github.com/en/issues/trying-out-the-new-projects-experience/about-projects>)
-/// and [GitHub settings](<https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features>).
-pub enum ProjectOrganisation {}
-
-/// Continuous Integration via [GitHub Actions](<https://github.com/actions>).
-pub enum SettingUpCI {}
-
-/// Web application deployment using [Netlify](<https://www.netlify.com>).
-pub enum WebDeployment {}
-
 // Technical skills
 
-/// Yew.
-pub const FRAMEWORKS: () = ();
-
-/// Arch Linux, Pop!_OS.
+/// Ubuntu, Darwin.
 pub const OS: () = ();
 
-/// Rust, Bash.
+/// Golang, Rust, Java.
 pub const PROGRAMMING_LANGUAGES: () = ();
 
-/// French (5. Native), English (3. Minimum Professional Proficiency).
+/// Chinese(5.Native) English (3. Minimum Professional Proficiency).
 pub const SPOKEN_LANGUAGES: () = ();
 
-/// Git, WASM, CLI, Terminal UI.
+/// Git, Docker, .
 pub const TECHNOLOGIES: () = ();
 
 // Personal traits
@@ -144,7 +81,5 @@ pub trait Flexible {}
 pub trait Passionate {}
 
 pub trait TeamPlayer {}
-
-pub trait WantToBuild {}
 
 pub trait WantToLearn {}
